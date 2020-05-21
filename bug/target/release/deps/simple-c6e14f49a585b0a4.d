2af97249ea9e7fad1c1b839d8adbf21b2a90bca9
simdax <simdax@DESKTOP-EH1MCM1.localdomain>
2020-05-21 15:39:53 +0200

    ouech

--- bug/target/release/deps/simple-c6e14f49a585b0a4.d
+++
@@ -1,5 +0,0 @@
-/home/simdax/strange/bug/target/release/deps/libsimple-c6e14f49a585b0a4.a: src/lib.rs
-
-/home/simdax/strange/bug/target/release/deps/simple-c6e14f49a585b0a4.d: src/lib.rs
-
-src/lib.rs:
