baf1603cbccba7c4075a26d2a7f5c5e88da4ec0e
simdax <simdax@DESKTOP-EH1MCM1.localdomain>
2020-05-21 15:38:57 +0200

    add makefi

---
+++ bug/target/release/deps/simple-c6e14f49a585b0a4.d
@@ -0,0 +1,5 @@
+/home/simdax/strange/bug/target/release/deps/libsimple-c6e14f49a585b0a4.a: src/lib.rs
+
+/home/simdax/strange/bug/target/release/deps/simple-c6e14f49a585b0a4.d: src/lib.rs
+
+src/lib.rs:
